4d7bf7c6600a2a1500585b379f28ecdb3ecc2de8
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-13 04:42:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function saturdayFun(activity = "roller-skate") {
   return `This Saturday, I want to ${activity}!`;
 }
 
-let mondayWork = function(activity) {
+let mondayWork = function(activity = "go to the office") {
   return `This Monday, I will ${activity}.`
 }
